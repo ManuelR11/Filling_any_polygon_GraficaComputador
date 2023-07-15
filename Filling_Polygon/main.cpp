2d7f67a665cfc8a1bd070efb2d95b7ef9ce5b98a
ManuelR11 <rod21509@uvg.edu.gt>
2023-07-14 18:22:25 -0600

    Poligono 5

--- Filling_Polygon/main.cpp
+++ Filling_Polygon/main.cpp
@@ -10,36 +10,6 @@ void render() {
     Framebuffer framebuffer(800, 600, Color(0, 0, 0)); // Crear instancia de Framebuffer con ancho, alto y color de borrado
     framebuffer.setCurrentColor(Color(255, 255, 255)); // Establecer color actual para los puntos y líneas
 
-    
-    // Polígono 4
-    Point::Vertex2 vertices4[] = {
-            {413, 177},
-            {448, 159},
-            {502, 88},
-            {553, 53},
-            {535, 36},
-            {676, 37},
-            {660, 52},
-            {750, 145},
-            {761, 179},
-            {672, 192},
-            {659, 214},
-            {615, 214},
-            {632, 230},
-            {580, 230},
-            {597, 215},
-            {552, 214},
-            {517, 144},
-            {466, 180}
-    };
-
-    framebuffer.setCurrentColor(Color(255, 255, 255)); // Establecer color blanco para la orilla
-    Poligono::drawPoligono(vertices4, sizeof(vertices4) / sizeof(vertices4[0]), framebuffer);
-
-    framebuffer.setCurrentColor(Color(0, 255, 0)); // Establecer color verde para el relleno del polígono
-    Relleno::fillPolygon(vertices4, sizeof(vertices4) / sizeof(vertices4[0]), framebuffer);
-
-       /*
 
     // Polígono 5
     Point::Vertex2 vertices5[] = {
@@ -54,8 +24,7 @@ void render() {
 
     framebuffer.setCurrentColor(Color(0, 0, 0)); // Establecer color negro para el relleno del polígono
     Relleno::fillPolygon(vertices5, sizeof(vertices5) / sizeof(vertices5[0]), framebuffer);
-
-     */
+    
     framebuffer.renderBuffer(); // Generar el archivo BMP
 }
 
